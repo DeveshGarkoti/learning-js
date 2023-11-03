0f5243188dad103d2b440986232b96607ded76e5
Devesh Garkoti <87602664+DeveshGarkoti@users.noreply.github.com>
2023-11-03 14:31:56 +0000

    02_datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newer version
+
+// alert("hello") // we are using node JS not Browser
+
+console.log(5+6); // Code readeability should be High
+
+console.log("devesh");
+
+let name = "devesh"
+let age = 12
+let isLoggedIn = false
+let state = null
+
+// number => 2 to power 53
+// bigInt => 
+// string => ""
+// boolean => true/false
+// null => standalone value (represenatation of empty value)
+// undefined => value is not defined yet
+// symbol => unique
+
+// objects
+
+console.log(typeof(null)); // objects
+console.log(typeof(undefined)); // undefined
